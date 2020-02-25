c8316d5346285443977b8172f9e082524573a2ab
Cartucha <55837747+Cartucha@users.noreply.github.com>
2020-02-25 23:32:45 +0100

    Estructuras

---
+++ simple-webapp/src/test/java/rodriguez100361/AppTest.java
@@ -0,0 +1,20 @@
+package rodriguez100361;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
