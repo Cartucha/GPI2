4a4975aaacd4f1b3c65326d19b7a07bb9b393e5c
Cartucha <izaskun_r.v@gotmail.com>
2020-03-20 14:45:38 +0000

    cambios en proyecto simple

---
+++ simple/src/main/java/t4/demo/App.java
@@ -0,0 +1,15 @@
+package t4.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int[] num = {1,2,3,4};
+        System.out.println( "Hello World!" );
+        System.out.println( "......" + num[6]);
+    }
+}
---
+++ simple/src/test/java/t4/demo/AppTest.java
@@ -0,0 +1,23 @@
+package t4.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void MainTest()
+    {
+        App nueva = new App();
+        App.main(new String[]{});
+       
+    }
+}
+
